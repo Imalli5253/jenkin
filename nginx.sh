07b6ea865d28a2aacb9b3ab443567818bc332db6
Imalli5253 <pnr.759@gmail.com>
2019-08-03 13:59:25 +0530

    Remove

--- nginx.sh
+++ nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update
-yum install httpd -y 
-service httpd enable 
-service httpd restart
+yum remove httpd -y 
+#service httpd enable 
+#service httpd restart
 #service restart nginx
